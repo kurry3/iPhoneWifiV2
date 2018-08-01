28cbb96ad42109d777a257cba2d88ce6aabca4c1
kurry <leong3@purdue.edu>
2018-07-31 17:13:03 -0700

    Update IPhoneWifi.java

--- IPhoneWifi.java
+++ IPhoneWifi.java
@@ -1,102 +1,108 @@
 package iOSWifi3;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.*;
-import org.testng.annotations.Test;
+
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-import org.openqa.selenium.Dimension;
-//import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.ios.IOSDriver;
-
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 
-public class AppiumParallelTest implements Runnable{
-	
+
+public class iPhoneWifi implements Runnable{
+
 	 String url;
 	 String udid;
 	 String deviceName;
 	 String devicePort;
-	   
-	    public AppiumParallelTest(String url, String udid, String deviceName, String devicePort) {
+	 String bootstrapPort;
+	 Integer wdaLocalPort;
+	 String deviceType;
+
+	    public iPhoneWifi(String url, String udid, String deviceName, Integer wdaLocalPort, String deviceType) {
 	        this.url = url;
 	        this.udid = udid;
 	        this.deviceName = deviceName;
-	        this.devicePort = devicePort;
+	        this.wdaLocalPort = wdaLocalPort;
+		this.deviceType = deviceType;
 	    }
-	   
+
 	    IOSDriver<MobileElement> driver;
 	    DesiredCapabilities cap = new DesiredCapabilities();
-	   
-	   
-	    private void controlCenter() {
+
+	    @SuppressWarnings("rawtypes")
+		private void controlCenter() {
 			cap.setCapability("platformName", "iOS");
-			cap.setCapability("platformVersion", "11.3");
+			cap.setCapability("platformVersion", "11.4");
 			cap.setCapability("deviceName", deviceName);
 			cap.setCapability("automationName", "XCUITest");
 			cap.setCapability("udid", udid);
+			cap.setCapability("wdaLocalPort", wdaLocalPort);
 			cap.setCapability("bundleId", "com.yourcompany.Endpoint");
-			//cap.setCapability("useNewWDA", true);
-			cap.setCapability("wdaLocalPort", devicePort);
-	       
-	        try {
-	            driver = new IOSDriver<MobileElement>(new URL(url), cap);
-	            Thread.sleep(10000);
-	        } catch (MalformedURLException e) {
-	            e.printStackTrace();
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-	       
-	        System.out.println(url + " Test Started");
-			//System.out.println(tldriver.get().manage());
+
+			try {
+				driver = (new IOSDriver<MobileElement>(new URL(url),cap));
+			}catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+
+	     		try {
+				Thread.sleep(5*1000);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+
+	        	System.out.println(url + " Test Started");
 			Dimension d = driver.manage().window().getSize();
 			int height = d.getHeight();
 			int width = d.getWidth();
-			System.out.println(height);
-			System.out.println(width);
+			//System.out.println(height);
+			//System.out.println(width);
 
 			TouchAction touchAction = new TouchAction(driver);
-			touchAction.press(width/2, height).waitAction(Duration.ofMillis(500)).moveTo(width/2, -height).release().perform();
-
+			//iPhoneX control center is accessed by pulling down from the top right corner of the screen
+			if (deviceType.equals("iPhoneX")){
+				System.out.println("an iPhoneX");
+				touchAction.press(PointOption.point(width, 0)).waitAction(WaitOptions.waitOptions(Duration.ofMillis(500))).moveTo(PointOption.point(width/2, height/2)).release().perform();
+			}else{
+			//control center is accessed by pulling up from the bottom of the screen
+				System.out.println(deviceType);
+				touchAction.press(PointOption.point(width/2, height)).waitAction(WaitOptions.waitOptions(Duration.ofMillis(500))).moveTo(PointOption.point(width/2, -height)).release().perform();
+			}
 			try {
 				Thread.sleep(2*1000);
 			}catch(InterruptedException e){
 				e.printStackTrace();
 			}
 
-			int count = 0;
+			driver.findElementByAccessibilityId("wifi-button").click();
+			try {
+				Thread.sleep(2*1000);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
 
-			do {
-				driver.findElementByAccessibilityId("wifi-button").click();
-				count = count + 1;
-				try {
-					Thread.sleep(6000);
-				}catch(InterruptedException e){
-					e.printStackTrace();
-				}
-				System.out.println(count);
-			}while (count <= 5);
-			System.out.println(deviceName + " Test Complete" );
 			driver.quit();
+			System.out.println("DONE");
 	    }
-	   
+
 	    public static void main(String args[]) {
-	    	Runnable r1 = new AppiumParallelTest("http://127.0.0.1:3001/wd/hub", "0a1364a47e9b044d605558130817ad53a9b5208c", "RF's iPhone7", "8300"); //device id of first mobile device
-	        System.out.println("iPhone7 Instance Created" );
-	        Runnable r2 = new AppiumParallelTest("http://127.0.0.1:3005/wd/hub", "f2164f6fed275ba15386c7eebc99b2926ad398c6", "RF's iPhone", "8200"); //device id of second mobile device
-	        System.out.println("iPhone8 Instance Created");
-	        new Thread(r1).start();
+	        //Runnable r2 = new AppiumParallelTest("http://127.0.0.1:3005/wd/hub", "f2164f6fed275ba15386c7eebc99b2926ad398c6", "RF's iPhone", "8200"); //device id of second mobile device
+		//host url, udid, "device name", wdalocalport, "deviceType NO SPACE" ie: iPhoneX
+		Runnable r2 = new iPhoneWifi(args[0], args[1], args[2], Integer.valueOf(args[3]), args[4]);
+	    	System.out.println("iPhone Instance Created");
 	        new Thread(r2).start();
 	    }
-	 
+
 	    @Override
 	    public void run() {
 	        controlCenter();
+
 	    }
 	}
-
